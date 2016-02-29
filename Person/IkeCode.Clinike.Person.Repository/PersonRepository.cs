90ec8bcca84351a151f4185db511f2442e631484
Leandro Barral <ikecode@gmail.com>
2016-02-29 15:36:57 -0300

    renomeado pasta local do repository

---
+++ Person/IkeCode.Clinike.Person.Repository/PersonRepository.cs
@@ -0,0 +1,15 @@
+﻿using IkeCode.Clinike.Person.Domain.Repository;
+using IkeCode.Data.Core.Repository;
+using System.Data.Entity;
+
+namespace IkeCode.Clinike.Person.Repository
+{
+    public class PersonRepository : IkeCodeRepositoryBase<Person, Domain.Entities.IPerson, int>, IPersonRepository
+    {
+        public PersonRepository(DbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
