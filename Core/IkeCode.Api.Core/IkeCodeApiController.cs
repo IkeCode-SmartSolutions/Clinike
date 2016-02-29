7309ee7f610ab43b0a0d3a3acf3ad3d63099577b
Leandro Barral <ikecode@gmail.com>
2016-02-29 17:46:19 -0300

    Runner controllerbase

--- Core/IkeCode.Api.Core/IkeCodeApiController.cs
+++ Core/IkeCode.Api.Core/IkeCodeApiController.cs
@@ -6,5 +6,30 @@
 
     public class IkeCodeApiController : ApiController
     {
+        protected IIkeCodeApiResponse<TResponse> Run<TResponse>(Func<TResponse> func)
+        {
+            try
+            {
+                var response = func();
+                return new IkeCodeApiResponse<TResponse>(IkeCodeResponseStatus.Success, response);
+            }
+            catch (Exception ex)
+            {
+                return new IkeCodeApiResponse<TResponse>(IkeCodeResponseStatus.Error, ex.Message);
+            }
+        }
+
+        protected async Task<IIkeCodeApiResponse<TResponse>> RunAsync<TResponse>(Func<Task<TResponse>> func)
+        {
+            try
+            {
+                var response = await func();
+                return new IkeCodeApiResponse<TResponse>(IkeCodeResponseStatus.Success, response);
+            }
+            catch (Exception ex)
+            {
+                return new IkeCodeApiResponse<TResponse>(IkeCodeResponseStatus.Error, ex.Message);
+            }
+        }
     }
 }
--- Person/IkeCode.Clinike.Person.Api/Controllers/PersonController.cs
+++ Person/IkeCode.Clinike.Person.Api/Controllers/PersonController.cs
@@ -1,17 +1,12 @@
-﻿using IkeCode.Api.Core;
-using IkeCode.Clinike.Person.Domain.Entities;
-using IkeCode.Clinike.Person.Domain.Repository;
-using IkeCode.Data.Core.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Web.Http;
-
-namespace IkeCode.Clinike.Person.Api.Controllers
+﻿namespace IkeCode.Clinike.Person.Api.Controllers
 {
+    using IkeCode.Api.Core;
+    using Domain.Entities;
+    using Domain.Repository;
+    using Data.Core.Model;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+
     public class PersonController : IkeCodeApiController
     {
         IPersonRepository _personRepository;
@@ -24,28 +19,37 @@ namespace IkeCode.Clinike.Person.Api.Controllers
         [HttpGet]
         public async Task<IIkeCodeApiResponse<IPerson>> Get(string name, string include = "")
         {
-            var person = await _personRepository.FindAsync(i => i.Name == name, includes: include);
-            return new IkeCodeApiResponse<IPerson>(IkeCodeResponseStatus.Success, person);
+            return await RunAsync(async () =>
+            {
+                return await _personRepository.FindAsync(i => i.Name == name, includes: include);
+            });
         }
 
         [HttpGet]
         public async Task<IIkeCodeApiResponse<IPagedResult<IPerson>>> Get(int offset = 0, int limit = 10, string include = "")
         {
-            var personList = await _personRepository.GetAsync(offset, limit, includes: include, orderBy: i => i.Name);
-            return new IkeCodeApiResponse<IPagedResult<IPerson>>(IkeCodeResponseStatus.Success, personList);
+            return await RunAsync(async () =>
+            {
+                return await _personRepository.GetAsync(offset, limit, includes: include);
+            });
         }
 
         [HttpGet]
         public async Task<IIkeCodeApiResponse<IPerson>> Get(int id, string include = "")
         {
-            var person = await _personRepository.FindAsync(i => i.Id == id, includes: include);
-            return new IkeCodeApiResponse<IPerson>(IkeCodeResponseStatus.Success, person);
+            return await RunAsync(async () =>
+            {
+                return await _personRepository.FindAsync(i => i.Id == id, includes: include);
+            });
         }
 
         [HttpPost]
         public async Task<IIkeCodeApiResponse<int>> Post([FromBody]IPerson person)
         {
-            return await Task.Run(() => { return new IkeCodeApiResponse<int>(IkeCodeResponseStatus.Success, 1); });
+            return await RunAsync(async () =>
+            {
+                return await Task.Run(() => { return 1; });
+            });
         }
 
         [HttpPut]
@@ -53,7 +57,7 @@ namespace IkeCode.Clinike.Person.Api.Controllers
         {
             await Task.Run(() => { _personRepository.Update(id, person); });
         }
-        
+
         [HttpDelete]
         public async Task Delete(int id)
         {
