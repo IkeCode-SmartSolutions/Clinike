ef3cbcf1c7b7b09e5b19362728b0b5536d27f9fb
Leandro Barral <ikecode@gmail.com>
2016-02-29 15:19:38 -0300

    adicionado classe de person

---
+++ Person/IkeCode.Clinike.Person.Repository.MySql/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace IkeCode.Clinike.Person.Repository
+{
+    using IkeCode.Clinike.Person.Domain.Entities;
+    using IkeCode.Data.Core.Model;
+
+    public class Person : IkeCodeModel<int>, IPerson
+    {
+        public Person()
+        {
+        }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
