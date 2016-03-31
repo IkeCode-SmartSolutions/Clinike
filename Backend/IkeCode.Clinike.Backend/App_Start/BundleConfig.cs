1cfa5a02d247589cc0b492d517780f2f0268b23c
Leandro Barral <ikecode@gmail.com>
2016-03-31 15:40:32 -0300

    added styles and script to bundle

---
+++ Backend/IkeCode.Clinike.Backend/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using IkeCode.Web.Core.Mvc;
+using System.Web.Optimization;
+
+namespace IkeCode.Clinike.Backend
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            var jsBundles = new ScriptBundle("~/jsBundles/js");
+            jsBundles.Orderer = new AsDefinedBundleOrderer();
+            //jsBundles.Include("~/Scripts");
+            jsBundles.Include("~/Scripts/bootstrap.min.js");
+            jsBundles.Include("~/Scripts/pixel-admin.js");
+            bundles.Add(jsBundles);
+
+            var styleBundle = new IkeCodeStyleBundle("~/cssBundles/styles");
+            styleBundle.Orderer = new AsDefinedBundleOrderer();
+            //styleBundle.Include("~/Content/Css", new CssRewriteUrlTransform());
+            styleBundle.Include("~/Content/Css/bootstrap.css", new CssRewriteUrlTransform());
+            styleBundle.Include("~/Content/Css/pixel-admin.css", new CssRewriteUrlTransform());
+            styleBundle.Include("~/Content/Css/widgets.min.css", new CssRewriteUrlTransform());
+            styleBundle.Include("~/Content/Css/rtl.css", new CssRewriteUrlTransform());
+            styleBundle.Include("~/Content/Css/themes.css", new CssRewriteUrlTransform());
+            bundles.Add(styleBundle);
+        }
+    }
+}
