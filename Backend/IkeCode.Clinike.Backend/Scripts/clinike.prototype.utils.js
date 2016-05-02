590a191e4e0e6315095a813d14799ad7e9a61b51
Leandro Barral <ikecode@gmail.com>
2016-05-02 17:05:00 -0300

    switcher custom binding working and applied to phonedetail

--- Backend/IkeCode.Clinike.Backend/Scripts/clinike.prototype.utils.js
+++ Backend/IkeCode.Clinike.Backend/Scripts/clinike.prototype.utils.js
@@ -16,32 +16,32 @@ try {
 
 ko.bindingHandlers.switcher = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        $log.verbose('ko.bindingHandlers.switcher :: init.element', element);
-        $log.verbose('ko.bindingHandlers.switcher :: init.valueAccessor', valueAccessor);
+        var value = ko.utils.unwrapObservable(valueAccessor());
+        //$log.verbose('ko.bindingHandlers.switcher :: init.value unwraped', value);
+        //$log.verbose('ko.bindingHandlers.switcher :: init.element', element);
+        //$log.verbose('ko.bindingHandlers.switcher :: init.valueAccessor', valueAccessor);
         //$log.verbose('ko.bindingHandlers.switcher :: init.allBindings', allBindings);
         //$log.verbose('ko.bindingHandlers.switcher :: init.viewModel', viewModel);
         //$log.verbose('ko.bindingHandlers.switcher :: init.bindingContext', bindingContext);
 
         $elem = $(element);
         $(element).bootstrapSwitch();
-        $log.verbose('ko.bindingHandlers.switcher :: init.plugin');
-        $log.verbose('ko.bindingHandlers.switcher :: init.ko.utils.unwrapObservable(valueAccessor())', ko.utils.unwrapObservable(valueAccessor()));
-        $(element).bootstrapSwitch('setState', ko.utils.unwrapObservable(valueAccessor()));
+        $(element).bootstrapSwitch('state', value);
         $elem.on('switch-change', function (e, data) {
             valueAccessor()(data.value);
         });
     },
     update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        $log.verbose('ko.bindingHandlers.switcher :: update.element', element);
-        $log.verbose('ko.bindingHandlers.switcher :: update.valueAccessor', valueAccessor);
+        //$log.verbose('ko.bindingHandlers.switcher :: update.element', element);
+        //$log.verbose('ko.bindingHandlers.switcher :: update.valueAccessor', valueAccessor);
         //$log.verbose('ko.bindingHandlers.switcher :: update.allBindings', allBindings);
         //$log.verbose('ko.bindingHandlers.switcher :: update.viewModel', viewModel);
         //$log.verbose('ko.bindingHandlers.switcher :: update.bindingContext', bindingContext);
 
-        var vStatus = $(element).bootstrapSwitch('state');
-        var vmStatus = ko.utils.unwrapObservable(valueAccessor());
-        if (vStatus != vmStatus) {
-            $(element).bootstrapSwitch('setState', vmStatus);
+        var currentStatus = $(element).bootstrapSwitch('state');
+        var newStatus = ko.utils.unwrapObservable(valueAccessor());
+        if (currentStatus != newStatus) {
+            $(element).bootstrapSwitch('state', newStatus);
         }
     }
 };
